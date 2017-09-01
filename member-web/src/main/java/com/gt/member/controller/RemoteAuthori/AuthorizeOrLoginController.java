4b0c86481aebf74c7ebb1708cb43dee9e455825c
pengjianglilive@163.com <pjl734049>
2017-09-01 16:32:27 +0800

    修改

--- member-web/src/main/java/com/gt/member/controller/RemoteAuthori/AuthorizeOrLoginController.java
+++ member-web/src/main/java/com/gt/member/controller/RemoteAuthori/AuthorizeOrLoginController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSON;
 import com.gt.api.util.sign.SignHttpUtils;
 import com.gt.member.util.CommonUtil;
 import com.gt.member.util.MemberConfig;
@@ -39,11 +40,10 @@ public class AuthorizeOrLoginController {
 	Integer busId = CommonUtil.toInteger( map.get( "busId" ) );
 	Integer browser = CommonUtil.judgeBrowser( request );
 	Object uclogin = map.get( "uclogin" );
-	String wxmpSign = "WXMP2017";
 	Map< String,Object > getWxPublicMap = new HashMap<>();
 	getWxPublicMap.put( "busId", busId );
 	//判断商家信息 1是否过期 2公众号是否变更过
-	String wxpublic = SignHttpUtils.postByHttp(memberConfig.getWxmp_home()+GETWXPULICMSG, getWxPublicMap, wxmpSign );
+	String wxpublic = SignHttpUtils.postByHttp(memberConfig.getWxmp_home()+GETWXPULICMSG, getWxPublicMap, memberConfig.getWxmpsignKey() );
 	JSONObject json = JSONObject.parseObject( wxpublic );
 	Integer code = CommonUtil.toInteger( json.get( "code" ) );
 	if ( code == 0 ) {
@@ -53,12 +53,12 @@ public class AuthorizeOrLoginController {
 		Object guoqiUrl = json.get( "guoqiUrl" );
 		return "redirect:" + guoqiUrl;
 	    }
+
 	    Object remoteUcLogin = json.get( "remoteUcLogin" );
 	    if ( CommonUtil.isNotEmpty( uclogin ) || CommonUtil.isNotEmpty( remoteUcLogin ) ) {
 		return "";
 	    }
 	}
-
 	String requestUrl = CommonUtil.toString( map.get( "requestUrl" ) );
 	String otherRedisKey = CommonUtil.getCode();
 	redisCacheUtil.set( otherRedisKey, requestUrl, 5 * 60 );
@@ -67,7 +67,7 @@ public class AuthorizeOrLoginController {
 	queryMap.put( "browser", browser );
 	queryMap.put( "busId", busId );
 	queryMap.put( "uclogin", uclogin );
-	String url = "redirect:http://192.168.2.240:8080/remoteUserAuthoriPhoneController/79B4DE7C/authorizeMember.do?queryBody=" + queryMap;
+	String url = "redirect:"+memberConfig.getWxmp_home()+"/remoteUserAuthoriPhoneController/79B4DE7C/authorizeMember.do?queryBody=" + JSON.toJSONString(  queryMap);
 	return url;
     }
 }
