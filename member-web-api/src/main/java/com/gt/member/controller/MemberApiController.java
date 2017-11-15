7a4b438fddbccc2f00af34e4f9a0b9756c08db57
pengjianglilive@163.com <pjl734049>
2017-11-15 11:56:59 +0800

    修改

--- member-web-api/src/main/java/com/gt/member/controller/MemberApiController.java
+++ member-web-api/src/main/java/com/gt/member/controller/MemberApiController.java
@@ -1,9 +1,7 @@
 package com.gt.member.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.gt.api.enums.ResponseEnums;
-import com.gt.entityBo.PaySuccessBo;
 import com.gt.member.base.BaseController;
 import com.gt.member.dto.ServerResponse;
 import com.gt.member.entity.MemberCard;
@@ -20,7 +18,6 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
