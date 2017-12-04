3eb8fc41bd2763a4413b7f3ef8a105666015a03d
pengjianglilive@163.com <pjl734049>
2017-12-04 15:59:41 +0800

    修改

--- member-web/src/main/java/com/gt/member/controller/member_pc/MemberController.java
+++ member-web/src/main/java/com/gt/member/controller/member_pc/MemberController.java
@@ -577,11 +577,12 @@ public class MemberController {
     @ResponseBody
     @RequestMapping( value = "/findMemberCommend", method = RequestMethod.GET )
     public ServerResponse findMemberCommend(HttpServletRequest request,
-		    HttpServletResponse response,Map<String,Object> params){
+		    HttpServletResponse response,@RequestParam  String  params){
 	try {
+	    Map<String,Object> map=JSON.parseObject( params,Map.class );
 	    Integer busId = SessionUtils.getPidBusId( request );
-	    params.put( "recommendType",0 );
-	    Page page= memberCardService.findCommend(busId,params);
+	    map.put( "recommendType",0 );
+	    Page page= memberCardService.findCommend(busId,map);
 	    return ServerResponse.createBySuccess( page  );
 	} catch ( BusinessException e ) {
 	    return ServerResponse.createByError( e.getCode(), e.getMessage() );
