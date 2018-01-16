727291b31e3a0b977365099905ab42393e06387b
pengjianglilive@163.com <pjl734049>
2018-01-16 17:10:54 +0800

    修改

---
+++ member-dao/src/main/java/com/gt/member/dao/duofencard/DuofenCardTimeDAO.java
@@ -0,0 +1,16 @@
+package com.gt.member.dao.duofencard;
+
+import com.gt.member.entity.DuofenCardTime;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author pengjiangli
+ * @since 2018-01-16
+ */
+public interface DuofenCardTimeDAO extends BaseMapper<DuofenCardTime> {
+
+}
