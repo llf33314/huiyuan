7aefb3e3924d6f231f3c3618fbe641f1144ed50d
pengjianglilive@163.com <pjl734049>
2018-02-02 16:04:54 +0800

    修改

--- member-entity/src/main/java/com/gt/member/entity/MemberRecommend.java
+++ member-entity/src/main/java/com/gt/member/entity/MemberRecommend.java
@@ -30,9 +30,6 @@ public class MemberRecommend extends Model<MemberRecommend> {
 	@TableId(value="id", type= IdType.AUTO)
 	private Integer id;
 
-    	@TableField("busId")
-	private Integer busId;
-
     /**
      * 推荐人
      */
